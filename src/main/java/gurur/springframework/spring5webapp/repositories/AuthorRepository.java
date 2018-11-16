89e2a5146b6484426ac5dff1139dc65439072e29
gescalante-ml <gescalante.ml@gmail.com>
2018-11-15 23:57:15 -0200

    Spring jpa data

---
+++ src/main/java/gurur/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,12 @@
+package gurur.springframework.spring5webapp.repositories;
+
+import gurur.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+
+
+
+
+}
